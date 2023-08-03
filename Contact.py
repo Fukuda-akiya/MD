724356f066b13a374be55f79f12b53e89c52074a
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2023-08-03 18:07:34 +0900

    Add files via upload

--- Contact.py
+++ Contact.py
@@ -1,12 +1,40 @@
 import numpy as np
 import mdtraj as md
 import matplotlib.pyplot as plt
-from contact_map import ContactFrequency
+from contact_map import ContactFrequency, AtomMismatchedContactDifference
+import pandas as pd
 
-traj = md.load("/Users/fukuda/Kenkyu/OaPAC/dark_4YUS/Run/4YUS_dark_run_all.nc",
-		top = "/Users/fukuda/Kenkyu/OaPAC/dark_4YUS/Run/4YUS_dark_run_BB.prmtop")
-topology = traj.topology
+# resd trajctory and topology
+traj1 = md.load("/Users/fukuda/Kenkyu/OaPAC/dark_4YUS/Run/4YUS_dark_run_strip_comp10.nc",
+		top = "/Users/fukuda/Kenkyu/OaPAC/dark_4YUS/Run/4YUS_dark_run_strip.prmtop")
+traj2 = md.load("/Users/fukuda/Kenkyu/OaPAC/dark_4YUT/4YUT_dark_run_strip_all.nc",
+		top = "/Users/fukuda/Kenkyu/OaPAC/dark_4YUT/4YUT_dark_run_strip.prmtop")
+topology1 = traj1.topology	
+topology2 = traj2.topology
 
-traj_contacts = ContactFrequency(traj)
-fig, ax = traj_contacts.residue_contacts.plot(cmap='seismic', vmin=-1, vmax=1);
+# calculate contact
+resid1 = topology1.select("name CA")
+protein1 = topology1.select("resSeq 1 to 350 or resSeq 367 to 717 and symbol != 'H'")
+protein2 = topology2.select("resSeq 1 to 350 or resSeq 368 to 718 and symbol != 'H'")
+traj_contacts1 = ContactFrequency(traj1, query=protein1, haystack=protein1)
+traj_contacts2 = ContactFrequency(traj2, query=protein2, haystack=protein2)
+diff = AtomMismatchedContactDifference(traj_contacts1,traj_contacts2)
+
+f = open("diff_contact_frequency.dat","w")
+f.write(str(diff.residue_contacts.most_common()[:15]) + "\n")
+f.write("\n")
+f.write(str(list(reversed(diff.residue_contacts.most_common()))[:15]) + "\n")
+f.close()
+
+# plot
+fig, ax = diff.residue_contacts.plot(cmap='seismic', vmin=-1, vmax=1)
+colorbar_obj = fig.axes[1]
+colorbar_obj.yaxis.set_ticks(np.arange(-1.0,1.1,0.25))
+colorbar_obj.set_yticklabels(np.arange(-1.0,1.1,0.25), weight='bold',  size=13, fontname='Arial')
+ax.set_aspect('equal', adjustable='box')
+plt.xlabel("Residue number", fontsize=18, fontname='Arial', fontweight='bold')
+plt.ylabel("Residue number", fontsize=18, fontname='Arial', fontweight='bold')
+plt.xticks(fontsize=13, fontname='Arial', fontweight='bold')
+plt.yticks(fontsize=13, fontname='Arial', fontweight='bold')
+plt.savefig('Png/Diff_contact_map.png', bbox_inches='tight', pad_inches = 0, dpi=300)
 plt.show()
--- Pca.py
+++ Pca.py
@@ -6,6 +6,7 @@ import MDAnalysis.analysis.pca as pca
 import pandas as pd
 from sklearn.cluster import KMeans
 import seaborn as sns
+import nglview as nv
 
 # Make Universe object from traj and parm
 md_uni = mda.Universe("/Users/fukuda/Kenkyu/OaPAC/dark_4YUS/Run/4YUS_dark_run_BB_C_strip.prmtop",
@@ -29,30 +30,52 @@ alignment.run()
 
 # PCA of CA
 Ca_pca = pca.PCA(md_uni,
-		 select = ' name CA '
+		 select = 'backbone '
 		)
 Ca_pca.run()
 
 # decrease dimension for CA
-transformed = Ca_pca.transform(md_uni.select_atoms("name CA"), n_components=3)
+transformed = Ca_pca.transform(md_uni.select_atoms("backbone"), n_components=3)
 
-# Store to dataframe
-#df = pd.DataFrame(transformed, columns=['PC1', 'PC2', 'PC3'])
-#df['Time (ps)'] = df.index * frame_time
+# PC1
+pc_1 = Ca_pca.results.p_components[:,2]
 
+weight_1 = transformed[:,2]
+
+mean_coord = Ca_pca.mean
+
+pro_1 = np.outer(weight_1, pc_1) + mean_coord.flatten()
+
+coordinates = pro_1.reshape(len(weight_1),-1,3)
+print(coordinates.shape)
+
+# Make universe 
+pro_uni = mda.Merge(md_uni.select_atoms("backbone"))
+pro_uni.load_new(coordinates, order = "fac")
+pro_select = pro_uni.select_atoms("backbone")
+
+# traj output
+pro_select.write('PC3_backbone_all.nc', frames="all")
+
+# View NGL
+#view = nv.show_mdanalysis(pro_uni.atoms) 
+#view
+
+"""
 # k-means methods
 kmeans = KMeans(n_clusters=3, max_iter=30, init="random")
-cluster = kmeans.fit_predict(transformed[:,0:1])
+cluster = kmeans.fit_predict(transformed[:,0:1:2])
 df = pd.DataFrame(transformed, columns=['PC1', 'PC2', 'PC3'])
 df["cluster"] = cluster
-centers = kmeans.cluster_centers_
-#df_cluster_centers = pd.DataFrame(cluster_centers, columns=["x1", "x2"])
-print(centers[:,0] + centers[:,1])
-"""
+#centers = kmeans.cluster_centers_
+#df_cluster_centers = pd.DataFrame(centers, columns=["x", "y"])
+
+
 # plot clustering
 sns.scatterplot(data=df, x='PC1',y='PC2',hue="cluster")
 # plot cluster center
-sns.scatterplot(data=df_cluster_centers, x="x1", y="x2",  s=200, marker='*', color='gold', linewidth=0.5)
+#sns.scatterplot(data=df_cluster_centers, x="x", y="y", s=200, marker='*', color='gold', linewidth=0.5)
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1], s=200, marker='*', color='gold', linewidth=0.5)
 
 plt.title("Clustering in PCA of CA")
 #plt.xlabel("1st principal component[$\AA$]")

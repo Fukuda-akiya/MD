432a54b7e5f08bbaa77f4f3c18869dc7591c94e5
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2023-11-20 15:27:29 +0900

    Add files via upload

--- sort_rmsf_to_pdb.py
+++ sort_rmsf_to_pdb.py
@@ -3,11 +3,11 @@ import pandas as pd
 import itertools as it
 
 resnum = 701
+datanum = 2804
 rmsf = [0] * (resnum+1)
 rmsf_split = [] * 2804
 rmsf_save = 0.0
 rmsf_save2 = [0] * (resnum + 1)
-rmsf_save3 = 0.0
 
 with open("4YUS_holo/RMSF_data.dat","r") as frmsf:
      rmsf = frmsf.readlines()
@@ -18,7 +18,6 @@ for i in range(0,resnum):
     for j in range(0,4):
         rmsf_split.append(rmsf_save2[i])
 
-datanum = 2804
 pdb_data = []
 pdb_HET = [0] * datanum
 pdb_atom_name = [0] * datanum
@@ -52,15 +51,137 @@ for i in range(datanum):
     pdb_occupancy[i] = pdb_data_save.split()[9]
     pdb_atom[i] = pdb_data_save.split()[11]
 
-    if len(pdb_atom_name[i]) == 2:
-       print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+    if len(pdb_atom_index[i]) == 1:
+        if len(pdb_atom_name[i]) == 2:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
                       pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
                       pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
-                      pdb_occupancy[i] + "  " + rmsf_split[i] + "           " +
-                      pdb_atom[i])
-    else:
-       print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+        elif len(pdb_atom_name[i]) == 1:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "      " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
                       pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
                       pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
-                      pdb_occupancy[i] + "  " + rmsf_split[i] + "           " +
-                      pdb_atom[i])
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+    elif len(pdb_atom_index[i]) == 2:
+        if len(pdb_atom_name[i]) == 2:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+        elif len(pdb_atom_name[i]) == 1:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "     " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+    elif len(pdb_atom_index[i]) == 3:
+        if len(pdb_atom_name[i]) == 2:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+        elif len(pdb_atom_name[i]) == 1:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "    " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+    elif len(pdb_atom_index[i]) == 4:
+        if len(pdb_atom_name[i]) == 2:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "  " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+        elif len(pdb_atom_name[i]) == 1:
+            if len(pdb_resid[i]) == 1:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "   " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 2:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + "  " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+            elif len(pdb_resid[i]) == 3:
+                print(pdb_HET[i] + "   " + pdb_atom_index[i] + "  " + pdb_atom_name[i] + "   " + 
+                      pdb_resname[i] + " " + pdb_chain[i] + " " + pdb_resid[i] + "      " +
+                      pdb_x[i] + "  " + pdb_y[i] + "  " + pdb_z[i] + "  " +
+                      pdb_occupancy[i] + "  " + "{:.6}".format(rmsf_split[i]) + "           " + pdb_atom[i])
+
+

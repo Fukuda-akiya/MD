9a315016c082939c0e4c0401dfad2617de314b6f
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2024-06-13 15:31:26 +0900

    Add files via upload

--- Analysis/a3-a4_angle.py
+++ Analysis/a3-a4_angle.py
@@ -1,26 +1,12 @@
-import numpy as np
-import math
 import MDAnalysis as mda
-import matplotlib.pyplot as p
+from sklearn.decomposition import PCA
+import numpy as np
 
 np.set_printoptions(threshold=np.inf)
 
-# 次元数(x,y,z etc.)
-dim = 3
-# 読み込むparmtop,trajectory
-# 8qfg
-traj_name = "/home/users/dzn/Kenkyu/OaPAC/Analysis/8QFG/traj/8QFG_run01-05.nc"
-parm_name = "/home/users/dzn/Kenkyu/OaPAC/Analysis/8QFG/parmtop/updated_bb.prmtop"
-# WT dark
-#traj_name = "/home/users/dzn/Kenkyu/OaPAC/Analysis/dark/4YUS_holo/traj_files/4YUS_dark_run01-05_3us.nc"
-#parm_name = "/home/users/dzn/Kenkyu/OaPAC/Analysis/dark/4YUS_holo/parm_files/4YUS_dark_holo_updated_3us.prmtop"
-# select resid
-resid_bottom_a3 = "resid 103:107 453:457"
-resid_top_a3 = "resid 121:125 471:475"
-resid_bottom_a4A = "resid 127:129" 
-resid_top_a4A = "resid 134:136"
-resid_bottom_a4B = "resid 477:479" 
-resid_top_a4B = "resid 484:486"
+# トポロジーとトラジェクトリーのファイルパス
+topology = '/home/users/dzn/Kenkyu/OaPAC/Analysis/8QFG/parmtop/updated_bb.prmtop'
+trajectory = '/home/users/dzn/Kenkyu/OaPAC/Analysis/8QFG/traj/8QFG_run01-05.nc'
 
 #########################################
 ###############Function##################
@@ -28,50 +14,28 @@ resid_top_a4B = "resid 484:486"
 
 # Universeから座標をndarrayに取り出す関数
 def extract(name):
-    atom = mda_uni.select_atoms(name)
-    coord_from_mda = np.array([atom.positions for frame in mda_uni.trajectory])
+    atom = u.select_atoms(name)
+    coord_from_mda = np.array([atom.positions for frame in u.trajectory])
 
     return coord_from_mda
 
-# 原子数の取得
-def atom_number(name):
-    atom = mda_uni.select_atoms(name)
+# 二つのヘリックス間の重心ベクトルを計算する関数
+def center(data1,data2):
+    vector = np.zeros(data1.shape)
+    for i in range(data1.shape[0]):
+        for j in range(data1.shape[1]):
+            vector[i, j] = ( data2[i,j] - data1[i,j] ) * 0.5
 
-    return atom
+    return vector
 
-# 重心を計算する関数
-def center(coord,atom):
-    # set ndarray for center
-    center = np.zeros([len(coord),dim])
-    for i in range(len(coord)):
-        for j in range(len(atom)):
-            for k in range(dim):
-                center[i][k] += coord[i,j,k] / len(atom)
-
-    return center
-
-# 重心間のベクトルを計算する関数
-def diff(coord,centerA,centerB):
-    R = np.zeros([len(coord),dim])
-    for i in range(len(coord)):
-        for j in range(dim):
-            R[i][j] = centerA[i][j] - centerB[i][j]
-
-    return R
-
-#　角度の計算（内積）
-def inner(coord, R_a, R_b):
-    angle = np.zeros(len(coord))
-    norm_a = np.zeros(len(coord))
-    norm_b = np.zeros(len(coord))
-    for i in range(len(coord)):
-        # 内積の計算
-        dot_product = np.dot(R_a.T[:,i], R_b[i,:])
-        # ベクトルの大きさを計算
-        norm_a[i] = np.linalg.norm(R_a[i,:])
-        norm_b[i] = np.linalg.norm(R_b[i,:])
-        # 角度
-        angle[i] = np.arccos(dot_product / (norm_a[i] * norm_b[i])) * 180 / np.pi
+# 鈍角を鋭角に修正する関数
+def fix_angle(data):
+    angle = np.zeros(len(data))
+    for i in range(len(data)):
+        if data[i] > 90.0:
+           angle[i] = 180.0 - data[i]
+        else:
+           angle[i] = data[i]
 
     return angle
 
@@ -79,60 +43,65 @@ def inner(coord, R_a, R_b):
 ########################################
 ########################################
 
+# Universeを作成
+u = mda.Universe(topology, trajectory)
 
-# parmtopとtrajectoryの読み込み
-mda_uni = mda.Universe(parm_name,traj_name)
+# 交差するα-ヘリックスの選択（例：残基範囲を指定）
+helix1 = 'resid 103:126 and name CA'
+helix2 = 'resid 453:476 and name CA'
+a4a = 'resid 127:136 and name CA'
+a4b = 'resid 477:486 and name CA'
 
-# trajectoryを初期frameにセット
-mda_uni.trajectory[0]
 
-# MDAnalysisのUniverseオブジェクトから座標をnumpyのarrayに取り出す
-# a3 helix
-coord_from_mda_bottom_a3 = extract(resid_bottom_a3)
-coord_from_mda_top_a3 = extract(resid_top_a3)
-# a4 helix
-coord_from_mda_bottom_a4A = extract(resid_bottom_a4A)
-coord_from_mda_top_a4A = extract(resid_top_a4A)
-coord_from_mda_bottom_a4B = extract(resid_bottom_a4B)
-coord_from_mda_top_a4B = extract(resid_top_a4B)
+# 座標をNumPy配列に変換
+positions_helix1 = extract(helix1)
+positions_helix2 = extract(helix2)
+positions_a4a = extract(a4a)
+positions_a4b = extract(a4b)
 
-# 原子数の取得
-# a3 helix
-atom_number_bottom_a3 = atom_number(resid_bottom_a3)
-atom_number_top_a3 = atom_number(resid_top_a3)
-# a4 helix
-atom_number_bottom_a4A = atom_number(resid_bottom_a4A)
-atom_number_top_a4A = atom_number(resid_top_a4A)
-atom_number_bottom_a4B = atom_number(resid_bottom_a4B)
-atom_number_top_a4B = atom_number(resid_top_a4B)
+# 交差する2つのヘリックスの重心の計算
+center_vec = center(positions_helix1, positions_helix2)
 
-# 重心の計算
-# 配列の初期化
-center_bottom_a3 = np.zeros(len(coord_from_mda_bottom_a3))
-center_top_a3 = np.zeros(len(coord_from_mda_top_a3))
-center_bottom_a4A = np.zeros(len(coord_from_mda_bottom_a4A))
-center_top_a4A = np.zeros(len(coord_from_mda_top_a4A))
-center_bottom_a4B = np.zeros(len(coord_from_mda_bottom_a4B))
-center_top_a4B = np.zeros(len(coord_from_mda_top_a4B))
-# a3 helix
-center_bottom_a3 = center(coord_from_mda_bottom_a3,atom_number_bottom_a3)
-center_top_a3 = center(coord_from_mda_top_a3,atom_number_top_a3)
-# a4 helix
-center_bottom_a4A = center(coord_from_mda_bottom_a4A,atom_number_bottom_a4A)
-center_top_a4A = center(coord_from_mda_top_a4A,atom_number_top_a4A)
-center_bottom_a4B = center(coord_from_mda_bottom_a4B,atom_number_bottom_a4B)
-center_top_a4B = center(coord_from_mda_top_a4B,atom_number_top_a4B)
+# PCAの結果を保存するリスト
+principal_vectors_helix1 = []
+#principal_vectors_helix2 = []
+principal_vectors_a4a = []
+principal_vectors_a4b = []
 
-# 重心間のベクトルの計算
-# a3 helix
-R_a3 = diff(coord_from_mda_bottom_a3,center_top_a3,center_bottom_a3)
-R_a4A = diff(coord_from_mda_bottom_a4A,center_top_a4A,center_bottom_a4A)
-R_a4B = diff(coord_from_mda_bottom_a4B,center_top_a4B,center_bottom_a4B)
+for i in range(len(u.trajectory)):
+        # a3 ヘリックスの主軸
+        pca_helix1 = PCA(n_components=1)
+        pca_helix1.fit(center_vec[i])
+        principal_vector_helix1 = pca_helix1.components_[0]
+        principal_vectors_helix1.append(principal_vector_helix1)
+        # a4a ヘリックスの主軸
+        pca_a4a = PCA(n_components=1)
+        pca_a4a.fit(positions_a4a[i])
+        principal_vector_a4a = pca_a4a.components_[0]
+        principal_vectors_a4a.append(principal_vector_a4a)
+        # a4b ヘリックスの主軸
+        pca_a4b = PCA(n_components=1)
+        pca_a4b.fit(positions_a4b[i])
+        principal_vector_a4b = pca_a4b.components_[0]
+        principal_vectors_a4b.append(principal_vector_a4b)
 
-# 角度の計算
-angle_a3_a4A = inner(coord_from_mda_bottom_a3,R_a3,R_a4A)
-angle_a3_a4B = inner(coord_from_mda_bottom_a3,R_a3,R_a4B)
+# NumPy配列に変換
+principal_vectors_helix1 = np.array(principal_vectors_helix1)
+#principal_vectors_helix2 = np.array(principal_vectors_helix2)
+principal_vectors_a4a = np.array(principal_vectors_a4a)
+principal_vectors_a4b = np.array(principal_vectors_a4b)
 
-print("Frame" + "\t" + "a3-a4a" + "\t" + "a3-a4b")
-for i in range(len(angle_a3_a4A)):
-    print(str(i) + "\t" + str(angle_a3_a4A[i]) + "\t" + str(angle_a3_a4B[i]))
+angle_degrees = np.zeros(len(u.trajectory))
+angle_degrees_fix = np.zeros(len(u.trajectory))
+
+#ベクトル間の角度を計算
+for i in range(len(u.trajectory)):
+    dot_product = np.dot(principal_vectors_helix1[i], principal_vectors_a4b[i])
+    norm_helix1 = np.linalg.norm(principal_vectors_helix1[i])
+    norm_helix2 = np.linalg.norm(principal_vectors_a4b[i])
+    cos_theta = dot_product / (norm_helix1 * norm_helix2)
+    angle = np.arccos(cos_theta)  # ラジアンでの角度
+    angle_degrees[i] = np.degrees(angle)  # 度単位の角度
+    
+    angle_degrees_fix = fix_angle(angle_degrees)
+    print(str(i) + "\t" + f"{angle_degrees_fix[i]:.4f}")
